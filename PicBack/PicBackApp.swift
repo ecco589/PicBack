e673d3d4c3020a1bf6399b09a27932cce40234e5
Ecco <342301@whut.edu.cn>
2025-01-18 22:32:28 +0800

    Initial Commit

---
+++ PicBack/PicBackApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicBackApp.swift
+//  PicBack
+//
+//  Created by Ecco Liu on 2025/1/18.
+//
+
+import SwiftUI
+
+@main
+struct PicBackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
